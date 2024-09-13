8a650b464ac1b02919adcebf3a70e672f6fa5c9a
Ravi kumar <156693478+rkravi24@users.noreply.github.com>
2024-09-13 20:51:09 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //Treat all js code as newer version.
+
+// alert(3+3); 
+// this will not work because we are using node.js not a browser
+
+console.log(3+
+    3);   //code readability should be high
+
+console.log("Hello");
+
+//Data Types:
+let name = "Ravi";     
+let age = 23;           
+let isLoggedIn = false; 
+
+
+//String 
+//bigint
+//number
+//boolean => true / false
+//null => standalone value
+//undefined
+//symbol => unique
+//object
+
+
+console.log(typeof null);   //object
+console.log(typeof undefined);   //undefined
+
+
+
+
+
