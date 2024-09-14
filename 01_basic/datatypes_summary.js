785b88804ec019be9260ddd510a5f5c66d708d6c
Ravi kumar <156693478+rkravi24@users.noreply.github.com>
2024-09-14 13:53:51 +0000

    memory in js

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -31,12 +31,39 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello World");
 }
-console.log(typeof myFunction); //function (object function)
+//console.log(typeof myFunction); //function (object function)
 
-console.log(typeof heros);      //object
+//console.log(typeof heros);      //object
 
 
 //typeof Values
 //https://262.ecma-international.org/5.1/#sec-11.4.3
 
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (primitive) 
+//Heap (Non-primitive)
+
+
+//Stack create copy of a data and share
+let name  = "Ravi";
+let anotherName = name;
+anotherName = "Tez";
+// console.log(name);
+// console.log(anotherName);
+
+
+//Heap memory share reference of same data
+//If we make changes to user2 then user1 will also be affected.
+let user1 = {
+    email: "raju@gmail.com",
+    upi: 1234,
+}
+
+let user2 = user1;
+user2.email = "rk123@gmail.com";
+console.log(user2);
+console.log(user1);
 
