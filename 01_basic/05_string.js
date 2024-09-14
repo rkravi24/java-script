26bfce0c5b546b876acd5c781882b327f696f441
Ravi kumar <156693478+rkravi24@users.noreply.github.com>
2024-09-14 21:41:53 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,27 @@
+const name = "Ravi";
+const count = 20;
+
+// console.log(name+count);
+
+console.log(`Hello my nam is ${name} and my repo count is 10`);
+
+const newName = new String(`Rkravi`);
+
+//string methods 
+
+// console.log(newName.length);
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt(1));
+// console.log(newName.indexOf(`v`));
+
+const newString = newName.substring(0,4);
+// console.log(newString);
+
+const anotherString = newName.slice(-6,3);
+// console.log(anotherString);
+
+
+const myName = "     Ravi           ";
+console.log(myName.trim());
+
+
