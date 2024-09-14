2c5de192f47815e00f497185ecdf52f2b47572a9
Ravi kumar <156693478+rkravi24@users.noreply.github.com>
2024-09-14 13:02:49 +0000

    datatypes summary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,8 @@
+//Mainly data types divided into two types(Based on how data stores in memory).
+// Primitive
+// Non-primitive
+
+// Primitive 
+// 7 Types : String, Number, Boolean, null, undefined, Symbol. BigInt 
+
+
