a919310051f4b477db2e1c9423c9a2715ad2ca42
Ravi kumar <156693478+rkravi24@users.noreply.github.com>
2024-09-19 14:24:13 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,15 @@
+
+for(let index = 0; index < 10; index++){
+    if(index == 5){
+        // console.log("5 is the best number");
+    }
+    // console.log(index);
+}
+
+
+for(let i = 0; i <= 10; i++){
+    console.log(`Outer loop value: ${i}`);
+    for(let j = 0; j <= 10; j++){
+        console.log(`inner loop value: ${j}`);
+    }
+}
